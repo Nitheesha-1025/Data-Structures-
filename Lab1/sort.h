043258d10370334491a1d52de131337aeed87305
Nitheesha-1025 <nitheeshamoturu@gmail.com>
2025-05-04 16:29:18 +0530

    Create sort.h

---
+++ Lab1/sort.h
@@ -0,0 +1,60 @@
+
+#include<cstdio>
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Bubble Sort
+void bubblesort(int a[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    printArray(a, n);
+}
+
+// Selection Sort
+void selectionsort(int arr[], int n) {
+    int i, j, minIdx, temp;
+    for (i = 0; i < n - 1; i++) {
+        minIdx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        
+        if (minIdx != i) {
+            temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+    printArray(arr, n);
+}
+
+// Insertion Sort
+void insertionsort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = temp;
+    }
+    printArray(arr, n);
+}
